47c613bfef8668fc884e29894302c05eaeb77aca
devcr <ti>
2022-10-30 17:38:10 +0900

    add Chapter5, not completed

---
+++ Chapter5_Loop/dowhile.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string password;
+    do {
+        cout << "Please enter your password: ";
+        cin >> password;
+    } while (password != "foobar");
+    cout << "Welcome, you got the password right\n";
+}
---
+++ Chapter5_Loop/practice5_CumulateNumber.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 사용자가 입력하는 수를 누적하며 더하는 프로그램
+int main()
+{
+    int val1, total;
+    total = 0;
+
+    cout << "수를 입력하세요: ";
+    cin >> val1;
+    total += val1;
+
+    while (val1 != 0) {
+        cout << "수를 입력하세요: ";
+        cin >> val1;
+        total += val1;
+    }
+
+    cout << "누적된 값: " << total << "\n";
+}
